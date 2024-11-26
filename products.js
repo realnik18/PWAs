f4c8c913f6897c1461a854d3a3518d264edc0099
Nikita <svstnnk@gmail.com>
2024-11-26 15:10:38 +0400

    update

---
+++ products.js
@@ -0,0 +1,48 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "A 25 pound",
+        "price": 20.00,
+        "image": "cat.png",
+
+        "availableInventory": 10,
+        "rating": 2
+    },
+
+    { "id": 1002,
+       "title": "Cat Food, 30lb bag",
+        "description": "A 30 pound",
+        "price": 21.00,
+
+        "availableInventory": 9,
+        "rating": 3
+
+    },
+    { "id": 1003,
+        "title": "Cat Food, 31lb bag",
+        "description": "A 31 pound",
+        "price": 22.00,
+
+        "availableInventory": 8,
+        "rating": 4
+
+
+
+
+    },
+    { "id": 1004,
+    "title": "Cat Food, 20lb bag",
+    "description": "A 20 pound",
+    "price": 25.00,
+
+    "availableInventory": 8,
+    "rating": 3
+
+
+    },
+
+
+
+
+]
