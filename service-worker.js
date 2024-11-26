243fa900410aca3a7fda11e4786aabc68848a5b7
Nikita <svstnnk@gmail.com>
2024-11-26 16:17:54 +0400

    images

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'product.js',
+    'petstore.webmanifest',
+    'images/yarn.jpg',
+    'images/cat-litter.jpg',
+    'images/laser-pointer.jpg',
+    'images/cat-house.jpg',
+    'images/icon-store-512.png'
+];
+
+// Install Event: Cache the specified files
+self.addEventListener('install', function(e) {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[Service Worker] Caching app shell');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', function(e) {
+    console.log('[Service Worker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(thisCacheName) {
+                    if (thisCacheName !== cacheName) {
+                        console.log('[Service Worker] Removing old cache:', thisCacheName);
+                        return caches.delete(thisCacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Event: Serve cached files if available
+self.addEventListener('fetch', function(e) {
+    console.log('[Service Worker] Fetching:', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
